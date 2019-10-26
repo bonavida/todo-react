0b8b37c3ab9cc5a3e61c15b424af444507deaaf9
Diego <gecko.bona@gmail.com>
2019-10-26 15:07:51 +0200

    Refactor - passed logic to App

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,61 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import TodoListItem from './TodoListItem';
 
 import './TodoList.scss';
 
-const initialItems = [
-  { id: 1, value: "Learn React", done: false },
-  { id: 2, value: "Go shopping", done: true },
-  { id: 3, value: "Buy flowers", done: true },
-];
+const TodoList = ({items, removeItem, toggleTodoDone}) => {
 
-class TodoList extends Component {
-  state = {
-    items: [],
-  };
+  const renderItems = items.map((item, index) => (
+    <TodoListItem
+      key={`todo-list-item-${index}`}
+      item={item}
+      removeItem={removeItem}
+      toggleTodoDone={toggleTodoDone}
+    />
+  ));
 
-  componentDidMount() {
-    this.setState({ items: initialItems });
-  }
-
-  removeItem = (itemId) => {
-    const { items } = this.state;
-    const todoItems = items.filter(item => item.id !== itemId);
-
-    this.setState({ items: todoItems });
-  }
-
-  toggleTodoDone = (itemId) => {
-    const { items } = this.state;
-    const todoItems = [ ...items ];
-    const todoItem = todoItems.find(item => item.id === itemId);
-    if (todoItem) {
-      todoItem.done = !todoItem.done;
-      this.setState({ items: todoItems });
-    }
-  }
-
-  renderItems() {
-    const { items } = this.state;
-
-    return items.map((item, index) => (
-      <TodoListItem
-        key={`todo-list-item-${index}`}
-        item={item}
-        removeItem={this.removeItem}
-        toggleTodoDone={this.toggleTodoDone}
-      />
-    ));
-  }
-
-  render () {
-    return (
-      <ul className="todolist__container list-group">
-        { this.renderItems() }
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className="todolist__container list-group">
+      { renderItems }
+    </ul>
+  );
+};
 
 export default TodoList;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import TodoList from 'components/TodoList';
 
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>React ToDo List</h1>
-    <TodoList />
-  </div>
-);
+const initialItems = [
+  { id: 1, value: "Learn React", done: false },
+  { id: 2, value: "Go shopping", done: true },
+  { id: 3, value: "Buy flowers", done: true },
+];
+
+class App extends Component {
+  state = {
+    items: [],
+  };
+
+  componentDidMount() {
+    this.setState({ items: initialItems });
+  }
+
+  removeItem = (itemId) => {
+    const { items } = this.state;
+    const todoItems = items.filter(item => item.id !== itemId);
+
+    this.setState({ items: todoItems });
+  }
+
+  toggleTodoDone = (itemId) => {
+    const { items } = this.state;
+    const todoItems = [ ...items ];
+    const todoItem = todoItems.find(item => item.id === itemId);
+    if (todoItem) {
+      todoItem.done = !todoItem.done;
+      this.setState({ items: todoItems });
+    }
+  }
+
+  render() {
+    const { items } = this.state;
+
+    return (
+      <div className="App">
+        <h1>React ToDo List</h1>
+        <TodoList
+          items={items}
+          removeItem={this.removeItem}
+          toggleTodoDone={this.toggleTodoDone}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
