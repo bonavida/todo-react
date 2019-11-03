234e7651fcbdb57ba3efeba1f5d286a9f971d770
cesarandex <cesarandex@gmail.com>
2019-11-03 21:09:19 +0100

    Added useRef case

--- src/components/AddTodoItem/AddTodoItem.js
+++ src/components/AddTodoItem/AddTodoItem.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { keyCodes } from 'utils/constants';
 
 import './AddTodoItem.scss';
 
 const AddTodoItem = props => {
   const [itemValue, setItemValue] = useState('');
-  const inputRef = React.createRef();
+  const inputRef = useRef(null);
 
   useEffect(() => {
-    inputRef && inputRef.current && inputRef.current.focus();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    inputRef.current.focus();
   }, []);
 
   const changeHandler = event => {
